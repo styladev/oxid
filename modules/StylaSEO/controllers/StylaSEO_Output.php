824fd637df139b3eed1fbead8dd2bcbb1d74f2e3
ssachtleben <ssachtleben@gmail.com>
2016-10-13 10:30:48 +0200

    set proper status code

--- modules/StylaSEO/controllers/StylaSEO_Output.php
+++ modules/StylaSEO/controllers/StylaSEO_Output.php
@@ -65,8 +65,8 @@ class StylaSEO_Output extends oxUBase{
 
             $ret = $this->_util->getRemoteContent($this->_username, $this->_feed_params);
             $this->_ret = $ret;
-            if ($ret->status) {
-                oxRegistry::getUtils()->setHeader("HTTP/1.0 ".$ret->status);
+            if (isset($ret['status'])) {
+                oxRegistry::getUtils()->setHeader("HTTP/1.0 ".$ret['status']);
             }
 
             $this->setMetaDescription($ret['meta']['description']);
