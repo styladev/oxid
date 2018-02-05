106fd14613d05f09966974e0b21863aa6486e3d8
Amely Kling <akling@noriskshop.de>
2018-02-05 20:24:22 +0100

    Changes for V1.5.7

--- copy_to_modules/Styla/models/Styla_Articlelist.php
+++ copy_to_modules/Styla/models/Styla_Articlelist.php
@@ -2,104 +2,90 @@
 
 class Styla_Articlelist extends oxArticleList
 {
-
     /**
      * Loads all relevant articles into the current list object
      *
      * @param int    $currPage       The current page
      * @param int    $pageSize       The page size
      * @param string $skuFilter      A single SKU / OXARTNUM
-     * @param string $categoryFilter A single oxcategory ID
+     * @param string $categoryFilter A single oxCategory ID
      */
     public function loadArticles($currPage = 1, $pageSize = 10, $skuFilter = '', $categoryFilter = '')
     {
-        //has module?
-        $myConfig = $this->getConfig();
+        $config = $this->getConfig();
+        $db = oxDb::getDb();
 
         $this->_aArray = array();
         $currPage = $currPage - 1;
-        $values = array();
 
-        $sArticleTable = getViewName('oxarticles');
-        if ($myConfig->getConfigParam('blNewArtByInsert')) {
-            $sType = 'oxinsert';
-        } else {
-            $sType = 'oxtimestamp';
+        $join = '';
+        $articleTable = getViewName('oxarticles');
+        $type = 'oxtimestamp';
+        if ($config->getConfigParam('blNewArtByInsert')) {
+            $type = 'oxinsert';
         }
-        $sSelect = "SELECT $sArticleTable.* from $sArticleTable";
-        $sWhere = " WHERE $sArticleTable.oxparentid = '' AND " . $this->getBaseObject()->getSqlActiveSnippet() . " AND $sArticleTable.oxissearch = 1 ";
+
+        $select = "SELECT $articleTable.* from $articleTable";
+        // SMO-75: Explicitly NO active check on the articles
+        $where = " WHERE $articleTable.oxparentid = '' AND $articleTable.oxissearch = 1 ";
+
         if ($skuFilter) {
-            $sWhere .= " AND $sArticleTable.oxartnum=?";
-            $values[] = $skuFilter;
+            $where .= " AND $articleTable.oxartnum=" . $db->quote($skuFilter);
         }
+
         if ($categoryFilter) {
-            /**
-             * $oCatList = oxNew('oxcategorylist');
-             * $oCatList->buildTree($categoryFilter);
-             * $cat_arr = array();
-             * foreach($oCatList as $cat){
-             * $cat_arr[] = $cat->getId();
-             * }
-             * $catids = '"'.(implode('","',$cat_arr)).'"';
-             **/
+            $join = " JOIN oxobject2category o2c ON " . $articleTable . ".oxid = o2c.oxobjectid JOIN oxcategories cat on cat.oxid = o2c.oxcatnid";
 
-            $sJoin = " JOIN oxobject2category o2c ON " . $sArticleTable . ".oxid = o2c.oxobjectid JOIN oxcategories cat on cat.oxid = o2c.oxcatnid";
-            $sWhere .= ' AND cat.oxid IN(';
-            /**
-             * foreach($cat_arr as $cat){
-             * $sWhere .= '?,';
-             * $values[] = $cat;
-             * }
-             * $sWhere = rtrim($sWhere,',').')';
-             **/
+            // SMO-75: Search all subcategories of the given category
+            $searchSubCategories = $this->getConfig()->getConfigParam('styla_feed_search_subcategories');
+            if ($searchSubCategories) {
+                $allCategories = $this->_getSubCategories($categoryFilter);
+                $allCategories = $db->quoteArray($allCategories);
 
-            // Used this query instead of the oxid tree function cause they didnt return a proper tree ...
-            $sWhere .= "SELECT OXID from oxcategories WHERE OXID = '" . $categoryFilter . "' OR OXPARENTID = '" . $categoryFilter . "' OR OXROOTID = '" . $categoryFilter . "')";
-        }
-        $sOrder = " ORDER by $sArticleTable." . $sType . " DESC ";
-        if (!($iLimit = (int) $pageSize)) {
-            $iLimit = $myConfig->getConfigParam('iNrofNewcomerArticles');
-        }
-        $iStart = $currPage;
-        if ($iStart > 0) {
-            $iStart = $iStart * $iLimit;
-        }
-        $selectString = $sSelect . $sJoin . $sWhere . $sOrder;
-        $this->_aSqlLimit[0] = $iStart;
-        $this->_aSqlLimit[1] = $iLimit;
-        $this->selectString($selectString, $values);
-    }
-
-    public function selectString($sSql, $values = array())
-    {
-        startProfile("loadinglists");
-        $this->clear();
-
-        $oDb = oxDb::getDb(oxDb::FETCH_MODE_ASSOC);
-        if ($this->_aSqlLimit[0] || $this->_aSqlLimit[1]) {
-            $rs = $oDb->selectLimit($sSql, $this->_aSqlLimit[1], $this->_aSqlLimit[0], $values);
-        } else {
-            $rs = $oDb->select($sSql, $values);
-        }
-
-        if ($rs != false && $rs->recordCount() > 0) {
-            $oSaved = clone $this->getBaseObject();
-            while (!$rs->EOF) {
-                $oListObject = clone $oSaved;
-                $this->_assignElement($oListObject, $rs->fields);
-                $this->add($oListObject);
-                $rs->moveNext();
+                $where .= ' AND cat.oxid IN(' . implode(',', $allCategories) . ')';
+            } else {
+                $quotedCategory = $db->quote($categoryFilter);
+                // Used this query instead of the oxid tree function cause they didnt return a proper tree ...
+                $where .= " AND cat.oxid IN(SELECT OXID from oxcategories WHERE OXID = $quotedCategory OR OXPARENTID = $quotedCategory OR OXROOTID = $quotedCategory)";
             }
         }
-        stopProfile("loadinglists");
+
+        $orderBy = " ORDER by $articleTable." . $type . " DESC ";
+        if (!($limit = (int) $pageSize)) {
+            $limit = $config->getConfigParam('iNrofNewcomerArticles');
+        }
+        $start = $currPage;
+        if ($start > 0) {
+            $start = $start * $limit;
+        }
+
+        $selectString = $select . $join . $where . $orderBy;
+        $this->_aSqlLimit[0] = $start;
+        $this->_aSqlLimit[1] = $limit;
+        $this->selectString($selectString);
     }
 
-    public function add($oObject)
+    /**
+     * Returns array of all subcategories of the given category
+     *
+     * @param string $categoryId
+     * @return array
+     */
+    protected function _getSubCategories($categoryId)
     {
-        if ($oObject->getId()) {
-            $this->_aArray[$oObject->getId()] = $oObject;
-        } else {
-            $this->_aArray[] = $oObject;
-        }
+        $db = oxDb::getDb();
+        $subCategoriesQuery = 'SELECT OXID FROM oxcategories WHERE OXPARENTID in (%s)';
+
+        $allCategories = array($categoryId);
+        $categories = array($categoryId);
+
+        do {
+            $categories = $db->quoteArray($categories);
+            $categories = $db->getCol(sprintf($subCategoriesQuery, implode(', ', $categories)));
+
+            $allCategories = array_merge($allCategories, $categories);
+        } while (count($categories));
+
+        return $allCategories;
     }
 }
--- copy_to_modules/Styla/translations/de/StylaSEO_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$sLangName = "Deutsch";
-
-$aLang = array(
-    'charset'   => 'UTF-8',
-    'STYLA_SEO_ERROR_NOUSERNAME'        => 'Fehler: Styla/Amazine Username nicht konfiguriert.'
-);
