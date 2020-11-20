7134434b10e49a95d25c0ad133e1169f4489f5c1
ssachtleben <ssachtleben@gmail.com>
2020-11-20 11:19:55 +0100

    ST-2388 removed legacy css

--- copy_to_modules/Styla/controllers/Styla_Magazine.php
+++ copy_to_modules/Styla/controllers/Styla_Magazine.php
@@ -65,7 +65,6 @@ class Styla_Magazine extends oxUBase
             }
 
             $this->_aViewData['js_embed'] = $this->_oUtil->getJsEmbedCode($this->_sUsername, $this->_sSnippetURL);
-            $this->_aViewData['css_embed'] = $this->_oUtil->getCssEmbedCode($this->_sUsername, $this->_sSnippetURL);
             $this->_aViewData['styla_div'] = '<div id="stylaMagazine">'.$aContent['noscript_content'].'</div>';
             $this->_aViewData['meta_author'] = $aContent['meta']['author'];
             $this->_aViewData['meta'] = $this->_createHeaderHtml($aContent['meta']);
